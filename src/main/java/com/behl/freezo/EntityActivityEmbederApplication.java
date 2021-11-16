c2a57d4b0c8e5d8c8ef476133b0b0352e24b1d28
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-11-16 11:45:16 +0530

    init commit

---
+++ src/main/java/com/behl/freezo/EntityActivityEmbederApplication.java
@@ -0,0 +1,13 @@
+package com.behl.freezo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityActivityEmbederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityActivityEmbederApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/behl/freezo/EntityActivityEmbederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behl.freezo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityActivityEmbederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
