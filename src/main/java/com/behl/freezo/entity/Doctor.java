8a689518e1912678a8350b76515cbfc002edc727
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-11-17 14:34:52 +0530

    minor fix: added @data

--- src/main/java/com/behl/freezo/entity/Doctor.java
+++ src/main/java/com/behl/freezo/entity/Doctor.java
@@ -10,8 +10,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
 @Table(name = "doctors")
+@Data
 public class Doctor implements Serializable {
 
     private static final long serialVersionUID = -3522578734933700737L;
