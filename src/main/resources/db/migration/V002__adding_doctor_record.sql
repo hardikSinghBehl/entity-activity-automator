c0df8a179a9ca911013995de3b856a35fdf80967
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-11-17 18:00:58 +0530

    minor fix: name to emailId

--- src/main/resources/db/migration/V002__adding_doctor_record.sql
+++ src/main/resources/db/migration/V002__adding_doctor_record.sql
@@ -1,1 +1,1 @@
-INSERT INTO doctors VALUES ('2f0f7ec4-d594-462e-95b0-2198b38e7d5f','Otto Octavius', '$2a$10$ES44yFas35w1LRjpjbHK8uZMFM8.QjpW4/Q5mSxJNH.yJs1maLelq')
+INSERT INTO doctors VALUES ('2f0f7ec4-d594-462e-95b0-2198b38e7d5f','octavius@otto.com', '$2a$10$ES44yFas35w1LRjpjbHK8uZMFM8.QjpW4/Q5mSxJNH.yJs1maLelq')
