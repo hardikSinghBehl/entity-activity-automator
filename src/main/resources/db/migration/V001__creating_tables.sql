9e1e302329d3c774b4bb5d198313eb039f72a234
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-11-17 15:16:06 +0530

    added flyway migration scripts

---
+++ src/main/resources/db/migration/V001__creating_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE doctors (
+  id UUID PRIMARY KEY,
+  email_id VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE patients (
+  id UUID PRIMARY KEY,
+  full_name VARCHAR(100) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  created_by UUID NOT NULL, 
+  updated_at TIMESTAMP NOT NULL,
+  updated_by UUID NOT NULL
+);
+
+CREATE TABLE appointments (
+  id INTEGER PRIMARY KEY,
+  patient_id UUID NOT NULL,
+  scheduled_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  created_by UUID NOT NULL, 
+  updated_at TIMESTAMP NOT NULL,
+  updated_by UUID NOT NULL,
+  CONSTRAINT `appointment_fkey_patients` FOREIGN KEY (patient_id) 
+  REFERENCES patients (id)
+);
