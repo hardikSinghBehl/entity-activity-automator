a1d22d837e594733d7adde1a096ca04b1c7edd0e
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-11-23 12:55:46 +0530

    adding second user to test updated_by

--- src/main/resources/db/migration/V002__adding_doctor_record.sql
+++ src/main/resources/db/migration/V002__adding_doctor_record.sql
@@ -1,1 +1,5 @@
-INSERT INTO doctors VALUES ('2f0f7ec4-d594-462e-95b0-2198b38e7d5f','octavius@otto.com', '$2a$10$ES44yFas35w1LRjpjbHK8uZMFM8.QjpW4/Q5mSxJNH.yJs1maLelq')
+-- decoded-password = otto@123
+INSERT INTO doctors VALUES ('2f0f7ec4-d594-462e-95b0-2198b38e7d5f','octavius@otto.com', '$2a$10$ES44yFas35w1LRjpjbHK8uZMFM8.QjpW4/Q5mSxJNH.yJs1maLelq');
+
+-- decoded-password = harry@123
+INSERT INTO doctors VALUES ('7e9ceedf-156d-4e20-bdb5-ea3dd3c16a6a','norman@oscorp.com', '$2a$10$.NKlqP/CkBaXMKJw5nfzu.hCWbSb6VBeP.4SbSg.k00zXfrwJVkGS');
